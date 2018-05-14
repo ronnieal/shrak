cf03066c4ef9a9edd1d0fd64d8b2068515c4465f
Ronnie Alemany <ronnie.al.dt90@gmail.com>
2018-05-13 23:15:08 -0700

    comments

--- GameWindow/TextureManager.cpp
+++ GameWindow/TextureManager.cpp
@@ -1,10 +1,11 @@
 #include "TextureManager.h"
-
+//used to load textures from a temporary surface
 SDL_Texture* TextureManager::loadTexture(const char* texture, SDL_Renderer* ren) 
 {
 	SDL_Surface* tempSurface = IMG_Load(texture);
 	SDL_SetColorKey(tempSurface, SDL_TRUE, SDL_MapRGB(tempSurface->format, 255, 255, 255));
 	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, tempSurface);
+	//free memory
 	SDL_FreeSurface(tempSurface);
 
 	return tex;
--- GameWindow/TextureManager.h
+++ GameWindow/TextureManager.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "Game.h"
 #include "SDL_image.h"
-
+//used to load textures 
 class TextureManager
 {
 public:
